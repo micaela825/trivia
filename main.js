07888a91d1b5c3c286d4c81b50a322ff0f677671
Micaela O'Herron <micaela.oherron@gmail.com>
2019-01-11 09:12:31 -0500

    changed port for deployment

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const app = require("./server");
-const PORT = 1337;
+const PORT = process.env.PORT || 1337;
 
 app.listen(PORT, () =>
   console.log(`studiously serving silly sounds on port ${PORT}`)
